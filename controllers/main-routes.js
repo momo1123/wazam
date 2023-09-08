d43fb90f88e811c40dfb52748eade859fb543f00
ktortolini <134340991+ktortolini@users.noreply.github.com>
2023-09-07 20:01:52 -0500

    added boilerplate for the main routes file

--- controllers/main-routes.js
+++ controllers/main-routes.js
@@ -1,1 +1,44 @@
-// todo: code here
+// imports express module and user, post
+const router = require('express').Router();
+const { User, Post } = require('../models');
+
+// get all posts for homepage
+router.get('/posts', async (req, res) => {
+	try {
+		const data = await Post.findAll({
+			include: [
+				{
+					model: Post,
+					attributes: ['username', 'time-stamp', 'message'],
+				},
+			],
+		});
+
+		const posts = data.map((post) =>
+			post.get({ plain: true }),
+		);
+
+		res.render('homepage', {
+			posts,
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+});
+
+// gets one user
+router.get('/users/:id', async (req, res) => {
+	try {
+		const data = await User.findByPk(req.params.id);
+
+		const user = data.get({ plain: true });
+
+		res.render('user', { user });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+});
+
+module.exports = router;
