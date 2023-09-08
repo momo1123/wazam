8208ab4281e7e80c112fbdd3998f64e0493d02ed
ktortolini <134340991+ktortolini@users.noreply.github.com>
2023-09-07 19:43:00 -0500

    added the initial file for models, relationships between user and post

---
+++ models/index.js
@@ -0,0 +1,13 @@
+// imports user and post modules
+const Post = require('./Post.js');
+const User = require('./User');
+// defines one-to-many
+User.hasMany(Post, {
+	foreignKey: 'user_id',
+});
+// defines many-to-one
+Post.belongsTo(User, {
+	foreignKey: 'user_id',
+});
+
+module.exports = { User, Post };
