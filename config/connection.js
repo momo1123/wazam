833594acf102a3fcc4b9d9dc1eb0fa3fe9236468
momo1123 <momozjunk@gmail.com>
2023-09-07 21:18:39 -0500

    Adding changes for encryption

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,21 @@
-// imports sequelize module
 const Sequelize = require('sequelize');
-// loads environment variables from .env file
 require('dotenv').config();
 
-// creates Sequelize instance using .env file
-const sequelize = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_USER,
-	process.env.DB_PASSWORD,
-	{
-		host: 'localhost',
-		dialect: 'mysql',
-		port: 3306,
-	},
-);
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3301
+    }
+  );
+}
 
 module.exports = sequelize;
