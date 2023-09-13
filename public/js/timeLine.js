542b32cf78fed26ce1db9375649f457448386da2
Khalil Barry <khalilbarry@Khalils-MBP.attlocal.net>
2023-09-13 13:54:26 -0500

    Just inserting W3 blogpost template going to implement handlebars now.

--- public/js/timeLine.js
+++ public/js/timeLine.js
@@ -2,11 +2,11 @@ let data = {
 
     title : "WAZZAM",
     content: "Welcome to WAZZAM, where you can charge your social battery!",
-    date: getCurrentDate(),
+    date: currentDate(),
 };
 
 // Formatting the current date and making blog-post-template
-function currentDate() {
+function getCurrentDate() {
     var now = new Date()
     return now.toLocaleDateString("en-US",{
         year: "numeric",
@@ -17,21 +17,31 @@ function currentDate() {
 
 //rendering a new blog-post template when new post is made. still keeping the template the same
 var source = document.getElementById("wazzam-template").innerHTML;
-const template = Handle.compile(source);
+const template = Handlebars.compile(source);
 const html = template(date);
 
 document.getElementById("wazzam-container").innerHTML = html;
 
-data = {
-    title: newTitle, 
-    content: newContent,
-    date: currentDate(), //setting new date 
+function createNewBlogPost(newTitle, newcontent){
+    data = {
+        title: newTitle, 
+        content: newContent,
+        date: currentDate(), //setting new date 
+    
+};
+
+//  Re-render the blog post template
+let updatedHtml = template(data);
+
+// DOM insertion for updated HTML 
+
+document.getElementById("wazzam-container").innerHTML = updatedHtml;
 
 }
 
-var updatedHtml = template(data)
 
-// DOM insertion for updated HTML 
-document.getElementById("wazzam-container").innerHTML = updatedHtml;
+
+
+
 
 
