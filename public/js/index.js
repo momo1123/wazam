1813425995fdc4abd1616157fe38b6d53a120638
Khalil Barry <khalilbarry@Khalils-MBP.attlocal.net>
2023-09-12 23:17:44 -0500

    fixed a couple of my functions and variables such as my 'var' to a 'let'

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,11 @@
-var data = {
+let data = {
 
     title : "WAZZAM",
     content: "Welcome to WAZZAM, where you can charge your social battery!",
     date: getCurrentDate(),
 };
 
-// Formatting the current date
+// Formatting the current date and making blog-post-template
 function currentDate() {
     var now = new Date()
     return now.toLocaleDateString("en-US",{
@@ -15,7 +15,7 @@ function currentDate() {
     });
 }
 
-//rendering blog-post template
+//rendering a new blog-post template when new post is made. still keeping the template the same
 var source = document.getElementById("wazzam-template").innerHTML;
 const template = Handle.compile(source);
 const html = template(date);
@@ -23,5 +23,15 @@ const html = template(date);
 document.getElementById("wazzam-container").innerHTML = html;
 
 data = {
-    title
+    title: newTitle, 
+    content: newContent,
+    date: currentDate(), //setting new date 
+
 }
+
+var updatedHtml = template(data)
+
+// DOM insertion for updated HTML 
+document.getElementById("wazzam-container").innerHTML = updatedHtml;
+
+
