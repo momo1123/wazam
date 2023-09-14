942fdc7dbe63182dd3abfb208e157de7cc695b5e
ktortolini <134340991+ktortolini@users.noreply.github.com>
2023-09-14 14:34:26 -0500

    reformatted html file timeLine, replaced all spaces with tabs

--- public/js/timeLine.js
+++ public/js/timeLine.js
@@ -1,49 +1,44 @@
 let data = {
-
-    title : "WAZZAM",
-    content: "Welcome to WAZZAM, where you can charge your social battery!",
-    date: getCurrentDate(),
+   title: 'WAZZAM',
+   content: 'Welcome to WAZZAM, where you can charge your social battery!',
+   date: getCurrentDate(),
 };
 
 // Formatting the current date and making blog-post-template
 function getCurrentDate() {
-    var now = new Date()
-    return now.toLocaleDateString("en-US",{
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-    });
+   var now = new Date();
+   return now.toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+   });
 }
 
 //rendering a new blog-post template when new post is made. still keeping the template the same
-var source = document.getElementById("wazzam-template").innerHTML;
+var source = document.getElementById('wazzam-template').innerHTML;
 const template = Handlebars.compile(source);
 const html = template(date);
 
-document.getElementById("wazzam-container").innerHTML = html;
+document.getElementById('wazzam-container').innerHTML = html;
 
-function createNewBlogPost(newTitle, newContent){
-    data = {
-        title: newTitle, 
-        content: newContent,
-        date: getCurrentDate(), //setting new date 
-    
-};
+function createNewBlogPost(newTitle, newContent) {
+   data = {
+      title: newTitle,
+      content: newContent,
+      date: getCurrentDate(), //setting new date
+   };
 
-//  Re-render the blog post template
-let updatedHtml = template(data);
+   //  Re-render the blog post template
+   let updatedHtml = template(data);
 
-// DOM insertion for updated HTML 
+   // DOM insertion for updated HTML
 
-document.getElementById("wazzam-container").innerHTML = updatedHtml;
-
+   document.getElementById('wazzam-container').innerHTML = updatedHtml;
 }
 
 // new blog post in response to user action.
 
-createNewBlogPost("New Blog Post Title", "This is the content of the new blog post.");
-
-
-
-
-
+createNewBlogPost(
+   'New Blog Post Title',
+   'This is the content of the new blog post.'
+);
