bfa24a3f20a0a81606dd492350a9cd6c5b077d35
ktortolini <134340991+ktortolini@users.noreply.github.com>
2023-09-14 14:24:06 -0500

    changed the location of files merged into main

---
+++ controllers/mainRoutes.js
@@ -0,0 +1,54 @@
+// imports express module and user, post
+const router = require('express').Router();
+const { User, Post } = require('../models');
+const sequelize = require('../config/connection');
+
+// get all posts for homepage
+router.get('/', async (req, res) => {
+   try {
+      const data = await Post.findAll({
+         include: [
+            {
+               model: Post,
+               attributes: ['username', 'time-stamp', 'message'],
+               include: {
+                  model: User,
+                  attributes: ['username'],
+               },
+            },
+         ],
+      });
+
+      const postsFlat = data.map((post) => post.get({ plain: true }));
+
+      if (!req.session.user) {
+         res.render('home', {
+            posts: postsFlat,
+         });
+      } else {
+         res.render('home', {
+            posts: postsFlat,
+            user: req.session.user,
+         });
+      }
+   } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+   }
+});
+
+// gets one user
+router.get('/users/:id', async (req, res) => {
+   try {
+      const data = await User.findByPk(req.params.id);
+
+      const user = data.get({ plain: true });
+
+      res.render('user', { user });
+   } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+   }
+});
+
+module.exports = router;
