1710568f25976fe6892317b1b49e36716ba3f871
ktortolini <134340991+ktortolini@users.noreply.github.com>
2023-09-14 14:47:38 -0500

    changed line 5 in controllers/index.js, argument main-routes changed to mainRoutes

--- models/index.js
+++ models/index.js
@@ -3,11 +3,11 @@ const Post = require('./Post.js');
 const User = require('./User');
 // defines one-to-many
 User.hasMany(Post, {
-	foreignKey: 'user_id',
+   foreignKey: 'user_id',
 });
 // defines many-to-one
 Post.belongsTo(User, {
-	foreignKey: 'user_id',
+   foreignKey: 'user_id',
 });
 
 module.exports = { User, Post };
