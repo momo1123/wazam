623e6b1de9d25b4342fd41f6c9ac5bfc0895b5f0
ktortolini <134340991+ktortolini@users.noreply.github.com>
2023-09-16 20:01:54 -0500

    added the versious routes for comments and adding a post

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@
 const router = require('express').Router();
 // imports the main routes file
 // todo: code here if more routes
+const userRoutes = require('./userRoutes.js');
 const mainRoutes = require('./mainRoutes.js');
 // calls use() for all of the routes
 // todo: code here if more routes
+router.use('/', userRoutes);
 router.use('/', mainRoutes);
 
 module.exports = router;
--- controllers/mainRoutes.js
+++ controllers/mainRoutes.js
@@ -1,6 +1,6 @@
 // imports express module and user, post
 const router = require('express').Router();
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 // get the root page
@@ -10,25 +10,121 @@ router.get('/', (req, res) => {
 
 // get all posts for homepage
 router.get('/home', async (req, res) => {
+   console.log('session', req.session);
    try {
+      //  user_id: 1,
+
       const data = await Post.findAll({
-         include: {
-            model: User,
-         },
+         include: [
+            {
+               model: User,
+            },
+            {
+               model: Comment,
+            },
+         ],
       });
 
       const postsFlat = data.map((post) => post.get({ plain: true }));
 
-      if (!req.session.user) {
+      if (!req.session.user_id) {
          res.render('home', {
             posts: postsFlat,
          });
       } else {
+         console.log('2');
+
          res.render('home', {
             posts: postsFlat,
-            user: req.session.user,
+            user_id: req.session.user_id,
+            username: req.session.user_name,
          });
       }
+   } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+   }
+});
+
+router.get('/post/:id', async (req, res) => {
+   try {
+      const data = await Post.findByPk(req.params.id);
+
+      const post = data.get({ plain: true });
+
+      res.render('post', { post });
+   } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+   }
+});
+
+router.get('/create-post', (req, res) => {
+   res.render('create-post');
+});
+
+router.post('/post', async (req, res) => {
+   console.log('is data coming in', req.body);
+   try {
+      const data = await Post.create(req.body);
+
+      const post = data.get({ plain: true });
+
+      res.redirect('/home');
+   } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+   }
+});
+
+router.get('/create-comment/:post_id', async (req, res) => {
+   try {
+      const data = await Post.findByPk(req.params.post_id, {
+         include: {
+            model: Comment,
+         },
+      });
+      const post = data.get({ plain: true });
+
+      console.log('post:', post);
+
+      res.render('create-comment', {
+         comments: post.comments,
+         post_id: req.params.post_id,
+      });
+
+      //const postsFlat = data.map((post) => post.get({ plain: true }));
+   } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+   }
+});
+
+router.get('/comment/:post_id', async (req, res) => {
+   try {
+      const data = await Post.findByPk(req.params.post_id, {
+         include: {
+            model: Comment,
+         },
+      });
+
+      //const postsFlat = data.map((post) => post.get({ plain: true }));
+
+      res.json(data);
+   } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+   }
+});
+
+router.post('/comment', async (req, res) => {
+   console.log('is data coming in', req.body);
+   try {
+      const data = await Comment.create(req.body);
+
+      const comment = data.get({ plain: true });
+
+      res.redirect('/home');
    } catch (err) {
       console.log(err);
       res.status(500).json(err);
@@ -52,20 +148,20 @@ router.get('/users/:id', async (req, res) => {
 router.get('/loginPage', (req, res) => {
    // If the user is already logged in, redirect the request to another route
    if (req.session.logged_in) {
-     res.redirect('/home');
-     return;
+      res.redirect('/home');
+      return;
    }
- 
+
    res.render('login');
- });
+});
 // User signup route if already existed
- router.get('/signup', (req, res) => {
+router.get('/signup', (req, res) => {
    // If the user is already logged in, redirect the request to another route
    if (req.session.exists) {
-     res.redirect('/home');
-     return;
+      res.redirect('/home');
+      return;
    }
- 
+
    res.render('signup');
- });
+});
 module.exports = router;
