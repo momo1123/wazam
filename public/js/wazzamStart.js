fea6469c9ca590899ada4d73d42942b461b4adb5
ktortolini <134340991+ktortolini@users.noreply.github.com>
2023-09-17 15:30:12 -0500

    changed calculation for subtle animation to account for mouse starting at center of screen on sing up button click

--- public/js/wazzamStart.js
+++ public/js/wazzamStart.js
@@ -5,15 +5,27 @@ const superman =
 const container =
    document.querySelector('.backdrop-01') ||
    document.querySelector('.backdrop-02');
+let delayListener;
 // adds an event listener for movement of the mouse
-container.addEventListener('mousemove', (event) => {
-   // this is for debugging purposes
-   console.log('mousemove, (event) => {...}');
-   // grabs the speed attribute from superman object
-   const speed = superman.getAttribute('data-speed');
-   // calculates the x and y coordinates using the speed
-   const x = (window.innerWidth - event.pageX * speed) / 100;
-   const y = (window.innerHeight - event.pageY * speed) / 100;
-   // applies a translate method for the x and y coordinates
-   superman.style.transform = `translateX(${x}px) translateY(${y}px)`;
-});
+const attachListener = () => {
+   container.addEventListener('mousemove', (event) => {
+      // this is for debugging purposes
+      console.log('mousemove, (event) => {...}');
+      // grabs the speed attribute from superman object
+      const speed = superman.getAttribute('data-speed');
+      // important: do not change the decimal value
+      // calculates the x and y coordinates using the speed
+      const x = ((window.innerWidth / 2 - event.clientX * 0.95) * speed) / 100;
+      const y = ((window.innerHeight / 2 - event.clientY * 0.95) * speed) / 100;
+      // applies a translate method for the x and y coordinates
+      superman.style.transform = `translateX(${x}px) translateY(${y}px)`;
+   });
+};
+// starts a delay when the page loads
+const startDelay = () => {
+   delayListener = setTimeout(() => {
+      attachListener();
+   }, 200);
+};
+
+startDelay();
